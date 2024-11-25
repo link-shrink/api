0d4e46028b389993a9660cb9c03b109422fc36e8
Akbar <akbarjonjorayev001@gmail.com>
2024-11-25 16:44:54 +0500

    app fixed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn shrinker:app --host=0.0.0.0 --port=${PORT:-5000}
+uvicorn shrinker:app --host=0.0.0.0 --port=10000
--- firestoreDB/init_firestore.py
+++ firestoreDB/init_firestore.py
@@ -2,10 +2,12 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
-cred = credentials.Certificate(os.getenv("PATH_TO_SERVICE_ACCOUNT_KEY"))
+firebase_json = os.getenv('FIREBASE_INIT')
+cred = credentials.Certificate(json.loads(firebase_json))
 
 firebase_admin.initialize_app(cred)
 
